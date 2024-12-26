c0a299bbceea84da9c5a442879e86d2a2838ed18
diceboi <szasz.szabolcs1995@gmail.com>
2024-12-26 10:08:07 +0100

    no quesetion

--- app/components/Felhasznalo.jsx
+++ app/components/Felhasznalo.jsx
@@ -172,19 +172,6 @@ export default function Felhasznalo({ pageRef }) {
           value={telefonszam || ""}
           onChange={(e) => setTelefonszam(e.target.value)}
         />
-        <select
-          name="telefonszam"
-          className="w-full lg:w-1/2 bg-[--antracit] border border-[--white-border] rounded-full px-4 py-2 text-white"
-          placeholder="Válassz finanszírozási forrást"
-          value={finanszirozas || ''}
-          onChange={(e) => {setFinanszirozas(e.target.value), console.log(finanszirozas)}}
-        >
-          <option value="">Válassz finanszírozási formát</option>
-          <option value="Pályázat - céges">Céges pályázat</option>
-          <option value="Pályázat - lakossági">Lakossági pályázat</option>
-          <option value="Banki hitel">Banki hitel</option>
-          <option value="Önerő">Önerő</option>
-        </select>
         <div className="flex flex-nowrap items-start gap-2 lg:w-1/2 w-full mt-4">
           <input
             type="checkbox"
