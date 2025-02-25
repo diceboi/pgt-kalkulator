90db7f4c2cb8a164caad9d3bc6257ac56183b33f
diceboi <szasz.szabolcs1995@gmail.com>
2025-02-25 13:14:42 +0100

    fullblack logos

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,17 +1,43 @@
+"use client"
+
 import Image from "next/image"
 import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import Paragraph from "./Typo/Paragraph";
 import Button from "./Typo/Button";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation"
+import { useState, useEffect } from "react"
 
 export default function Footer() {
+
+    const searchParams = useSearchParams()
+    const [kampany, setKampany] = useState()
+
+    useEffect(() => {
+        const kampanyParam = searchParams.get("kampany");
+        if (kampanyParam.includes("fb") || kampanyParam.includes("fullblack")) {
+            let kampanyValue = "";
+            kampanyValue = "fb";
+        } else {
+            let kampanyValue = "";
+            kampanyValue = "pgt"
+
+          setKampany(kampanyValue);
+          console.log("Beállított kampány érték:", kampanyValue);
+        }
+      }, [searchParams, setKampany]);  
+
     return(
         <>
         <div className="flex flex-col bg-[--black] border-t border-[--white-border] px-4 py-16 text-white">
             <div className="container m-auto flex lg:flex-row flex-col lg:gap-0 gap-10">
 
-                <div className="flex flex-col gap-10 w-1/2">
-                    <Image src="/pgt-white-01.svg" height={100} width={91} alt="Profi Greentech logo" className="lg:w-[91px] w-[85px] h-auto" />
+                <div className="flex flex-col gap-10 lg:w-1/2">
+                    {kampany === "pgt" ? (
+                        <Image src="/pgt-white-01.svg" height={100} width={91} alt="Profi Greentech logo" className="w-[91px] h-auto" />
+                    ):(
+                        <Image src="/fullblack-logo.png" height={150} width={150} alt="Fullblack logo" className="w-[150px] h-auto" />
+                    )} 
                     <Paragraph>Irányítsd velünk a klímaváltozást</Paragraph>
                     <div className="flex flex-nowrap gap-4">
                         <FaFacebookF className="text-[--yellow] min-w-6 min-h-6"/>
@@ -19,7 +45,7 @@ export default function Footer() {
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-8 w-1/4">
+                <div className="flex flex-col gap-8 lg:w-1/4">
                     <Button>Gyors elérés</Button>
                     <ul className="flex flex-col gap-3">
                         <Link href="https://profigreentech.hu/termekek" className="hover:text-[--green] font-extralight"><li>Termékek</li></Link>
@@ -31,7 +57,7 @@ export default function Footer() {
                     </ul>
                 </div>
 
-                <div className="flex flex-col gap-8 w-1/4">
+                <div className="flex flex-col gap-8 lg:w-1/4">
                     <Button>Kapcsolat</Button>
                     <div className="flex flex-col gap-3">
                         <Paragraph>
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 import H1 from "./Typo/H1"
 import H2 from "./Typo/H2"
@@ -7,15 +9,40 @@ import Button from "./Typo/Button"
 import Link from "next/link"
 import { HiArrowSmLeft } from "react-icons/hi";
 import { HiChevronLeft } from "react-icons/hi";
+import { useSearchParams } from "next/navigation"
+import { useState, useEffect } from "react"
 
 
 export default function Nav() {
+
+    const searchParams = useSearchParams()
+    const [kampany, setKampany] = useState()
+
+    useEffect(() => {
+        const kampanyParam = searchParams.get("kampany");
+        if (kampanyParam.includes("fb") || kampanyParam.includes("fullblack")) {
+            let kampanyValue = "";
+            kampanyValue = "fb";
+        } else {
+            let kampanyValue = "";
+            kampanyValue = "pgt"
+
+          setKampany(kampanyValue);
+          console.log("Beállított kampány érték:", kampanyValue);
+        }
+      }, [searchParams, setKampany]);  
+
     return(
         <div className="sticky top-0 flex flex-col bg-[--black] w-full h-auto border-b border-[--white-border] px-4 z-50">
             <nav className="container m-auto flex flex-nowrap gap-4 min-h-[60px] items-center justify-between">
-                <Image src="/pgt-white-01.svg" height={100} width={91} alt="Profi Greentech logo" className="lg:w-[91px] w-[85px] h-auto" />
+                {kampany === "pgt" ? (
+                    <Image src="/pgt-white-01.svg" height={100} width={91} alt="Profi Greentech logo" className="lg:w-[91px] w-[85px] h-auto" />
+                ):(
+                    <Image src="/fullblack-logo.png" height={150} width={150} alt="Fullblack logo" className="lg:w-[150px] w-[85px] h-auto" />
+                )} 
                 <h4 className="text-white uppercase text-center leading-5 flex-grow">Napelem kalkulátor</h4>
-                <Link href="https://profigreentech.hu">
+                
+                <Link href={kampany === "pgt" ? 'https://profigreentech.hu' : 'https://fullblack.hu'}>
                     <Button classname="lg:flex hidden flex-nowrap gap-2 text-white hover:text-[--yellow] hover:underline"><HiArrowSmLeft />Vissza a főoldalra</Button>
                     <Button classname="lg:hidden flex flex-nowrap gap-2 text-white hover:text-[--yellow] hover:underline"><HiArrowSmLeft /></Button>
                 </Link>
