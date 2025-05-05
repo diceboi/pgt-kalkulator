3bfbcecb69aa189e6d2f0a66047e07b6c818a5ad
diceboi <szasz.szabolcs1995@gmail.com>
2025-05-05 12:16:06 +0200

    first try

--- app/components/Villanyszamla.jsx
+++ app/components/Villanyszamla.jsx
@@ -3,28 +3,15 @@
 import { useState, useContext, useEffect } from "react";
 import { Context } from "../Context";
 import { motion } from "framer-motion";
-import H3 from "./Typo/H3";
 import Paragraph from "./Typo/Paragraph";
-import Button from "./Typo/Button";
-import SecondaryButton from "./UI/SecondaryButton";
 import MainButton from "./UI/MainButton";
-import Egtajak from "./Egtajak";
-import Hajlasszog from "./TetoParameterek";
-import Magassag from "./Magassag";
-import Cim from "./Cim";
-import Akkumulator from "./Igenyek";
-import Tulpanelezes from "./Tulpanelezes";
-import Felhasznalas from "./Felhasznalas";
-import { debounce } from "lodash";
 import BaseContainer from "./UI/BaseContainer";
 
-export default function Villanyszamla({ }) {
-  const [page, setPage] = useState("1");
+export default function Villanyszamla() {
   const [sliderValue, setSliderValue] = useState(0);
   const [showExtraInput, setShowExtraInput] = useState(false);
   const [typingTimeout, setTypingTimeout] = useState(null);
   const {
-    currentPage,
     addPage,
     villanyszamla,
     villanyszamlaUzleti,
@@ -41,34 +28,27 @@ export default function Villanyszamla({ }) {
     } else {
       setSliderValue(villanyszamlaUzleti || 0);
     }
-    // Sync slider value with context
-  });
+  }, [valaszto, villanyszamla, villanyszamlaUzleti]);
 
   const handleChange = (e) => {
     const value = e.target.value;
-    setNumber(value);
 
-    // Clear the existing timeout
     if (typingTimeout) {
       clearTimeout(typingTimeout);
     }
 
-    // Set a new timeout
     const timeout = setTimeout(() => {
-      addPage('3');
-      scrollToNext('3');
+      addPage("3");
+      scrollToNext("3");
       console.log("Triggered after last keystroke:", value);
-    }, 500); // 500ms delay
+    }, 500);
 
     setTypingTimeout(timeout);
   };
 
   useEffect(() => {
-    // Clean up timeout on component unmount
     return () => {
-      if (typingTimeout) {
-        clearTimeout(typingTimeout);
-      }
+      if (typingTimeout) clearTimeout(typingTimeout);
     };
   }, [typingTimeout]);
 
@@ -82,192 +62,183 @@ export default function Villanyszamla({ }) {
   };
 
   return (
+    <BaseContainer
+      title={
+        valaszto === "Lakossági"
+          ? "Mennyi a havi villanyszámlád?"
+          : "Mennyi a havi villamosenergia igényed?"
+      }
+      subtitle={"Használd a csúszkát vagy írd be a konkrét összeget."}
+    >
+      <div className="flex flex-col items-center justify-center gap-8 lg:min-w-[500px] min-w-full">
+        {valaszto === "Lakossági" && (
+          <>
+            <motion.input
+              type="range"
+              id="slider1"
+              min="0"
+              max="200000"
+              value={sliderValue}
+              onInput={(e) => {
+                const value = Number(e.target.value);
+                setSliderValue(value);
+                setVillanyszamla(value);
 
-        <BaseContainer title={valaszto === "Lakossági" ? "Mennyi a havi villanyszámlád?" : "Mennyi a havi villamosenergia igényed?"} subtitle={"Használd a csúszkát vagy írd be a konkrét összeget."}>
+                const percentage = (value / 200000) * 100;
+                const sliderElement = e.target;
+                sliderElement.style.setProperty("--value", `${percentage}%`);
+                const glowIntensity = Math.min((percentage / 100) * 1, 2);
+                const glowColor = `rgba(247, 200, 42, ${glowIntensity})`;
+                sliderElement.style.setProperty("--thumb-glow", glowColor);
 
-        <div className="flex flex-col items-center justify-center gap-8 lg:min-w-[500px] min-w-full">
-          {/* Sliders */}
-          {valaszto === "Lakossági" && (
-            <>
+                setShowExtraInput(value === 200000);
+              }}
+              className="slider w-1/2"
+            />
+
+            <div className="relative flex flex-col gap-8">
               <motion.input
-                type="range"
-                id="slider1"
+                type="number"
                 min="0"
                 max="200000"
                 value={sliderValue}
-                onInput={(e) => {
-                  const value = Number(e.target.value);
-                  setSliderValue(value);
-                  setVillanyszamla(value);
-                  console.log("Villanyszámla: ", villanyszamla)
-
-                  // Calculate the percentage and update the CSS variable
-                  const percentage = (value / 200000) * 100; // Assuming 200000 is the max value
-                  const sliderElement = e.target;
-                  sliderElement.style.setProperty("--value", `${percentage}%`);
-
-                  // Calculate dynamic glow intensity
-                  const glowIntensity = Math.min((percentage / 100) * 1, 2); // Scale glow intensity (0 to 1)
-                  const glowColor = `rgba(247, 200, 42, ${glowIntensity})`;
-
-                  // Update CSS variable for thumb glow
-                  sliderElement.style.setProperty("--thumb-glow", glowColor);
-
-                  // Show extra input
-                  if (value === 200000) {
-                    setShowExtraInput(true); // Show extra input if slider reaches max
-                  } else {
-                    setShowExtraInput(false); // Hide extra input otherwise
-                  }
-                }}
-                className="slider w-1/2"
-              />
-
-
-              {/* Input Field */}
-              <div className="relative flex flex-col gap-8">
-                <motion.input
-                  type="number"
-                  min="0"
-                  max="200000"
-                  value={sliderValue}
-                  onChange={(e) => {
+                onChange={(e) => {
+                  const inputValue = e.target.value;
+                  if (/^\d*$/.test(inputValue)) {
                     const newValue = Math.min(
-                      Math.max(Number(e.target.value), 0),
+                      Math.max(Number(inputValue), 0),
                       200000
                     );
                     setSliderValue(newValue);
                     setVillanyszamla(newValue);
-                    handleChange
-                    console.log("Villanyszámla: ", villanyszamla)
+                    handleChange(e);
+                    setShowExtraInput(newValue === 200000);
+                  }
+                }}
+                className="w-fit appearance-none bg-white border border-[--white-border] h-10 rounded outline-none text-center text-2xl text-[--black] py-2 pr-12"
+              />
+              <motion.label
+                htmlFor="slider"
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-[--black] z-10 w-fit h-auto"
+              >
+                Ft/hó
+              </motion.label>
+            </div>
+          </>
+        )}
 
-                    if (newValue === 200000) {
-                      setShowExtraInput(true);
-                    } else {
-                      setShowExtraInput(false);
-                    }
-                  }}
-                  className="w-fit appearance-none bg-white border border-[--white-border] h-10 rounded outline-none text-center text-2xl text-[--black] py-2 pr-12"
-                />
-                <motion.label
-                  htmlFor="slider"
-                  className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-[--black] z-10 w-fit h-auto"
-                >
-                  Ft/hó
-                </motion.label>
-              </div>
-            </>
-          )}
+        {valaszto === "Üzleti" && (
+          <>
+            <motion.input
+              type="range"
+              id="slider2"
+              min="0"
+              max="50000"
+              value={sliderValue}
+              onInput={(e) => {
+                const value = Number(e.target.value);
+                setSliderValue(value);
+                setVillanyszamlaUzleti(value);
 
-          {valaszto === "Üzleti" && (
-            <>
+                const percentage = (value / 50000) * 100;
+                const sliderElement = e.target;
+                sliderElement.style.setProperty("--value", `${percentage}%`);
+                const glowIntensity = Math.min((percentage / 100) * 1, 2);
+                const glowColor = `rgba(247, 200, 42, ${glowIntensity})`;
+                sliderElement.style.setProperty("--thumb-glow", glowColor);
+              }}
+              className="slider w-1/2"
+            />
+
+            <div className="relative flex flex-col gap-8">
               <motion.input
-                type="range"
-                id="slider2"
+                type="number"
                 min="0"
                 max="50000"
                 value={sliderValue}
-                onInput={(e) => {
-                  const value = Number(e.target.value);
-                  setSliderValue(value);
-                  setVillanyszamlaUzleti(value);
-                  console.log("Villanyszámla üzleti: ", villanyszamlaUzleti)
-
-                  // Calculate the percentage and update the CSS variable
-                  const percentage = (value / 50000) * 100; // Assuming 200000 is the max value
-                  const sliderElement = e.target;
-                  sliderElement.style.setProperty("--value", `${percentage}%`);
-
-                  // Calculate dynamic glow intensity
-                  const glowIntensity = Math.min((percentage / 100) * 1, 2); // Scale glow intensity (0 to 1)
-                  const glowColor = `rgba(247, 200, 42, ${glowIntensity})`;
-
-                  // Update CSS variable for thumb glow
-                  sliderElement.style.setProperty("--thumb-glow", glowColor);
-                }}
-                className="slider w-1/2"
-              />
-
-              {/* Input Field */}
-              <div className="relative flex flex-col gap-8">
-                <motion.input
-                  type="number"
-                  min="0"
-                  max="50000"
-                  value={sliderValue}
-                  onChange={(e) => {
+                onChange={(e) => {
+                  const inputValue = e.target.value;
+                  if (/^\d*$/.test(inputValue)) {
                     const newValue = Math.min(
-                      Math.max(Number(e.target.value), 0),
+                      Math.max(Number(inputValue), 0),
                       50000
                     );
                     setSliderValue(newValue);
                     setVillanyszamlaUzleti(newValue);
-                    handleChange
-                    console.log("Villanyszámla üzleti: ", villanyszamlaUzleti)
+                    handleChange(e);
+                    setShowExtraInput(false); // üzletinél nincs extra input
+                  }
+                }}
+                className="w-fit appearance-none bg-white border border-[--white-border] h-10 rounded outline-none text-center text-2xl text-[--black] py-2 pr-16"
+              />
+              <motion.label
+                htmlFor="slider"
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-[--black] z-10 w-fit h-auto"
+              >
+                kWh/hó
+              </motion.label>
+            </div>
+          </>
+        )}
 
-                    if (newValue === 50000) {
-                      setShowExtraInput(true);
-                    } else {
-                      setShowExtraInput(false);
-                    }
-                  }}
-                  className="w-fit appearance-none bg-white border border-[--white-border] h-10 rounded outline-none text-center text-2xl text-[--black] py-2 pr-16"
-                />
-                <motion.label
-                  htmlFor="slider"
-                  className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-[--black] z-10 w-fit h-auto"
-                >
-                  kWh/hó
-                </motion.label>
-              </div>
-            </>
-          )}
-
-          {/* Render dynamically based on `showExtraInput` */}
-          {showExtraInput & (valaszto === "Lakossági") && (
-            <>
-              <div className="flex flex-col gap-4 items-center py-10">
-                <Paragraph classname={"text-center text-white"}>
-                  Ha a havi villanyszámlád nagyobb mint 200 000 Ft<br></br> írd
-                  be ide az összeget.
-                </Paragraph>
-                <div className="relative flex flex-col gap-4 items-center">
-                  <motion.input
-                    id="slider200"
-                    name="slider200"
-                    type="number"
-                    min="0"
-                    max="9999999"
-                    className="w-fit appearance-none bg-[--antracit] border border-[--white-border] h-10 rounded outline-none text-center text-white text-2xl py-2 pr-12"
-                    onChange={(e) => (setVillanyszamla(e.target.value), setVillanyszamlaUzleti(''), setVillanyszamlanagy(''), handleChange, console.log("Villanyszámlák: ", villanyszamla), console.log("Villanyszámla: ", villanyszamlanagy) )}
-                  />
-                  <motion.label
-                    htmlFor="slider"
-                    className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-white z-10 w-fit h-auto"
-                  >
-                    Ft/hó
-                  </motion.label>
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-      <div className={`${villanyszamla || villanyszamlaUzleti || villanyszamlanagy == null ? 'flex' : 'hidden' } bottom-0 p-4 flex-col justify-center items-center`}>
-          <MainButton
-            onclick={() => {
-              if(valaszto === "Üzleti") {
-                addPage('3'), 
-                scrollToNext('3')
-              } else {
-                addPage('4'),
-                scrollToNext('4')
-              }
-            }}
-            classname={'animate-bounce'}
-          >
-            Tovább
-          </MainButton>
+        {showExtraInput && valaszto === "Lakossági" && (
+          <div className="flex flex-col gap-4 items-center py-10">
+            <Paragraph classname={"text-center text-white"}>
+              Ha a havi villanyszámlád nagyobb mint 200 000 Ft<br /> írd be ide
+              az összeget.
+            </Paragraph>
+            <div className="relative flex flex-col gap-4 items-center">
+              <motion.input
+                id="slider200"
+                name="slider200"
+                type="number"
+                min="0"
+                max="9999999"
+                className="w-fit appearance-none bg-[--antracit] border border-[--white-border] h-10 rounded outline-none text-center text-white text-2xl py-2 pr-12"
+                onChange={(e) => {
+                  const inputValue = e.target.value;
+                  if (/^\d*$/.test(inputValue)) {
+                    const numericValue = Number(inputValue);
+                    setVillanyszamla(numericValue);
+                    setVillanyszamlaUzleti("");
+                    setVillanyszamlanagy("");
+                    handleChange(e);
+                  }
+                }}
+              />
+              <motion.label
+                htmlFor="slider"
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-white z-10 w-fit h-auto"
+              >
+                Ft/hó
+              </motion.label>
+            </div>
+          </div>
+        )}
       </div>
-      </BaseContainer>
+
+      <div
+        className={`${
+          villanyszamla || villanyszamlaUzleti || villanyszamlanagy == null
+            ? "flex"
+            : "hidden"
+        } bottom-0 p-4 flex-col justify-center items-center`}
+      >
+        <MainButton
+          onclick={() => {
+            if (valaszto === "Üzleti") {
+              addPage("3");
+              scrollToNext("3");
+            } else {
+              addPage("4");
+              scrollToNext("4");
+            }
+          }}
+          classname={"animate-bounce"}
+        >
+          Tovább
+        </MainButton>
+      </div>
+    </BaseContainer>
   );
 }
